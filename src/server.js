ba4792d5c0ba5225dd717658c6052ef3afa5df4f
KostiZin <124085498+KostiZin@users.noreply.github.com>
2024-11-20 18:56:43 -0500

    Revert "Hw3 crud"

--- src/server.js
+++ src/server.js
@@ -2,9 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import 'dotenv/config';
-import contactsRouter from './routers/contacts.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
 export const setupServer = () => {
   const app = express();
 
@@ -20,11 +19,41 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.send({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
 
-  app.use(notFoundHandler);
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
 
-  app.use(errorHandler);
+    const contact = await getContactById(contactId);
+
+    if (contact === null) {
+      return res.status(404).send({ message: 'Contact not found' });
+    }
+    res.send({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).send({ status: 404, message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send({
+      status: 500,
+      message: 'Something went wrong',
+    });
+  });
 
   const PORT = Number(process.env.PORT) || 3000;
 
