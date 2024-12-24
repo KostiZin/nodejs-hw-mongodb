f2c5c338a55fcf4a01b70cc1b01eb41a11a2ee59
KostiZin <you@example.com>
2024-12-24 10:01:28 -0500

    initial

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import * as fs from 'node:fs';
 import express from 'express';
 import path from 'node:path';
 import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import swaggerUI from 'swagger-ui-express';
 import 'dotenv/config';
 import contactsRouter from './routers/contacts.js';
 import usersRouter from './routers/auth.js';
@@ -10,10 +12,16 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { authenticate } from './middlewares/authenticate.js';
 
+const swaggerDoc = JSON.parse(
+  fs.readFileSync(path.resolve('docs/swagger.json'), 'utf-8'),
+);
+
 const PORT = Number(process.env.PORT) || 3000;
 
 export const setupServer = () => {
   const app = express();
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
   app.use('/photos', express.static(path.resolve('src/public/photos')));
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,12 +3,13 @@ import Joi from 'joi';
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20).allow(null),
-  isFavourite: Joi.boolean().default(false),
+  email: Joi.string().email().min(3).max(20).allow(null, '').optional(),
+  isFavourite: Joi.boolean().default(false).optional(),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
     .required()
     .default('personal'),
+  photo: Joi.string().allow(null, '').optional(),
 });
 
 export const replaceContactSchema = Joi.object({
